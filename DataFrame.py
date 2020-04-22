f608f2dcd2eb8406d9c66b6d7c39d373482674f1
mahmoudelembaby <elembaby22@gmail.com>
2020-04-23 00:13:45 +0200

    First commit

---
+++ DataFrame.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://sammylee@localhost:5432/doublebagger')
+
+posts = pd.read_sql('select * from post_post', engine)
+
+companies = pd.read_sql('select * from post_company', engine)
+
+posts_companies = pd.merge(
+    companies, posts, left_on='id', right_on='company_id')
+
+posts_companies
